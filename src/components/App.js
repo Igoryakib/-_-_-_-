b0c4f36d89c80a806e08aed4949a29611fb54492
Igoryakib <Igoryakib@gmail.com>
2022-07-21 15:52:41 +0300

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
   getManagers,
   postManager,
@@ -6,53 +6,79 @@ import {
   putManager,
 } from "../utils/fetchApi";
 
-const  App = () => {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-    const [changedName, setChangedName] = useState("");
-    const [changedDescription, setChangedDescription] = useState("");
+const App = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [changedName, setChangedName] = useState("");
+  const [changedDescription, setChangedDescription] = useState("");
+  const [id, setId] = useState('');
   const userData = new FormData();
-  // useEffect(() => {
-  //   getManagers()
-  //     .then((data) => data)
-  //     .catch((error) => console.log(error));
-  // }, []);
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    userData.append("login", login);
+    userData.append("password", password);
     userData.append("name", name);
-    userData.append('description', description);
+    userData.append("description", description);
     postManager(userData);
-    setDescription('');
-    setName('');
+    setDescription("");
+    setName("");
+    setLogin("");
+    setPassword("");
   };
-    const onChangeHandler = (event) => {
-      event.preventDefault();
-      if (changedName) {
-        userData.append("name", changedName);
-      }
-      if (changedDescription) {
-        userData.append("description", changedDescription);
-      }
-      putManager(userData, 36);
-      setDescription("");
-      setName("");
-    };
+  const onChangeHandler = (event) => {
+    event.preventDefault();
+    if (changedName) {
+      userData.append("name", changedName);
+    }
+    if (changedDescription) {
+      userData.append("description", changedDescription);
+    }
+    putManager(userData, id);
+    setDescription("");
+    setName("");
+  };
   const onDeleteHandler = () => {
-    deleteManager()
+    deleteManager(id);
   };
   const onClickBtn = () => {
-        getManagers()
+    getManagers()
       .then((data) => data)
       .catch((error) => console.log(error));
   };
   return (
-    <>
+    <main className="main">
       <h1 className="App">Happy hacking !</h1>
       <button className="getManagersBtn" onClick={onClickBtn}>
-        Get managers :)
+        Get managers
       </button>
+      <input
+        className="input"
+        placeholder="set manager id"
+        type="number"
+        name="id"
+        value={id}
+        onChange={(event) => setId(event.currentTarget.value)}
+      />
       <div className="wrapperForm">
         <form onSubmit={onSubmitHandler} className="form">
+          <input
+            placeholder="enter login"
+            className="input"
+            onChange={(event) => setLogin(event.currentTarget.value)}
+            value={login}
+            type="text"
+            name="login"
+          />
+          <input
+            placeholder="enter password"
+            className="input"
+            onChange={(event) => setPassword(event.currentTarget.value)}
+            value={password}
+            type="text"
+            name="password"
+          />
           <input
             placeholder="enter name"
             className="input"
@@ -78,7 +104,7 @@ const  App = () => {
             placeholder="enter new name"
             className="input"
             onChange={(event) => setChangedName(event.currentTarget.value)}
-            value={name}
+            value={changedName}
             type="text"
             name="description"
           />
@@ -88,7 +114,7 @@ const  App = () => {
             onChange={(event) =>
               setChangedDescription(event.currentTarget.value)
             }
-            value={description}
+            value={changedDescription}
             type="text"
             name="description"
           />
@@ -100,8 +126,8 @@ const  App = () => {
       <button onClick={onDeleteHandler} className="deleteBtn" type="button">
         delete manager
       </button>
-    </>
+    </main>
   );
-}
+};
 
 export default App;
